d717247b41fdd7bd8f35ceefb29560d1c114361d
Tomas Cerda <Tomas.Cerda@ibm.com>
2018-05-30 11:46:53 -0300

    Quitando service worker

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import 'raf/polyfill';
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom';
-import registerServiceWorker from './registerServiceWorker';
 import { I18nextProvider } from 'react-i18next';
 import i18n from './i18n';
 import App from './App';
